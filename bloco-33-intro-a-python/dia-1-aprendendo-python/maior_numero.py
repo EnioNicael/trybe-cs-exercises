a6149d2b140e3c4d7d1bad64c662fb4ac73c1292
Enio Nicael <enionicael@gmail.com>
2021-09-15 19:25:14 -0300

    definir funcao que retorna maior numero

---
+++ bloco-33-intro-a-python/dia-1-aprendendo-python/maior_numero.py
@@ -0,0 +1,10 @@
+# Crie uma função que receba dois números e retorne o maior deles.
+
+def get_the_biggest_number (a, b):
+  if a > b:
+    return a
+
+  return b
+
+
+print(get_the_biggest_number(12, 8))
